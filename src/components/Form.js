5898e4652038016de85d1c7570c6c848326bdac3
crusiris <cruzsirys@gmail.com>
2020-04-27 14:56:24 -0400

    Componentes, FORM Y HEADER

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Form = () => {
+    return ( 
+        <div className="row">
+                <div className="col s12 m8 offset-m2">
+                    <form>
+                    <h2>Encuentra Noticias por categoria</h2>
+                    </form>
+                </div>
+        </div>
+     );
+}
+ 
+export default Form;
